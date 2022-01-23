6831f8baf8ce193afa95cdf4f528d0fca9ccb0f2
Łukasz Kot <lukaszkot03@gmail.com>
2022-01-23 21:32:14 +0100

    Added ajax communication prototype.

--- src/MyBudget/MyBudget.App/wwwroot/js/site.js
+++ src/MyBudget/MyBudget.App/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿let component = $(".category-component");
+if (component != null && component.length !== 0)
+{
+    component.click("focus",(e)=>{
+        $.ajax({
+            url: "/api/selects/test",
+            success: function (data)
+            {
+                console.log(data.test);
+            }
+        })
+        
+    })
+}
